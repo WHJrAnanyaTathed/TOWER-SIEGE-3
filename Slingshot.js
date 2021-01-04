2eeb920fa045672a3e3fe98f07e5c29f219ae433
WHJrAnanyaTathed <76829993+WHJrAnanyaTathed@users.noreply.github.com>
2021-01-04 18:38:33 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,34 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 1
+        }
+        this.sling = Constraint.create(options);
+        this.pointB=pointB;
+        World.add(world, this.sling);
+
+    }
+    fly(){
+        this.sling.bodyA =null;
+    }
+    display(){
+        if(this.sling.bodyA){
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+        
+        strokeWeight(4);
+        stroke("turquoise");
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+        }
+    function keyPressed() {
+            if (value === 0) {
+              value = 255;
+            } else {
+              value = 0;
+            }
+          }
+}
